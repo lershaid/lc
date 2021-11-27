1f801d9b91fc8f5c25f6eb13dbb42b7979092472
lershaid <sa6or@yahoo.com>
2021-11-27 14:31:45 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ utils/inspiredByLoot.ts
@@ -0,0 +1,98 @@
+import { Project } from "../types/interface";
+
+const cryptoodleLoot: Project = {
+    name: "Toodle Loot Bag",
+    description:
+        "10,000 programmable Loot Bags engineered by Cryptoodles used to advance the technology of civilizations.",
+    whatToDo: [
+        {
+            content: "Claim Free BAG",
+            url: "https://docs.cryptoodles.com/toodl-nfts/redeeming-loot-bag-nfts"
+        },
+        {
+            content: "Trade",
+            url: "https://opensea.io/collection/toodle-loot-bag"
+        }
+
+    ],
+    website: {
+        url: "https://cryptoodles.com"
+    },
+    twitter: {
+        url: "https://twitter.com/cryptoodles"
+    },
+    discord: {
+        url: "https://discord.com/invite/g9SnvzdFs2"
+    },
+    opensea: {
+        url: "https://opensea.io/collection/toodle-loot-bag"
+    },
+    contract: {
+        content: "0x2a16107ea0d28ae822168c8bb3ef5460b1924a78"
+    },
+    github: {
+        url: "https://github.com/cryptoodles"
+    }
+};
+
+const nProject: Project = {
+    name: "n Project",
+    description: "Randomized generated numbers stored on chain. Feel free to use n in any way you want.",
+    whatToDo: [
+        {
+            content: "Trade",
+            url: "https://opensea.io/collection/n-project"
+        },
+    ]
+};
+
+const treasure: Project = {
+    name: "Treasure",
+    description: "Treasure for Loot.",
+    whatToDo: [
+        {
+            content: "Trade",
+            url: "https://opensea.io/collection/treasure-for-loot"
+        },
+    ],
+    discord: {
+        url: "hhttps://discord.gg/GQkZys9ksx"
+    },
+    twitter: {
+        url: "https://twitter.com/Treasure_NFT"
+    }
+};
+const dopeWars: Project = {
+    name: "Dope Wars",
+    description: "Dope Wars. A community run project. Free and fair public launch.",
+    whatToDo: [
+        {
+            content: "Trade",
+            url: "https://opensea.io/collection/dope-v4"
+        },
+    ],
+    twitter: {
+        url: "https://twitter.com/thedopewars?lang=en"
+    }
+};
+
+const adventureCards: Project = {
+    name: "Adventure Cards",
+    description: "Loot bag, owner, and transfer subgraph by @shahruz",
+    whatToDo: [
+        {
+            content: "Trade",
+            url: "https://opensea.io/collection/adventure-cards"
+        },
+    ],
+    twitter: {
+        url: "https://discord.gg/D9jw8eyS5V"
+    },
+};
+export {
+    cryptoodleLoot,
+    nProject,
+    treasure,
+    dopeWars,
+    adventureCards
+}
